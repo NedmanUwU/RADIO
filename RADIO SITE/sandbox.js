ac033b17d84f5fa8e6b8a31f928d4d8bcd747124
Nedman <matilda@krimarc.se>
2023-11-22 18:24:48 +0100

    V1

--- RADIO SITE/sandbox.js
+++ RADIO SITE/sandbox.js
@@ -1,43 +1,44 @@
 const container = document.querySelector(".container");
 
+//fetches the data from the SR API
 async function getRadioStations() {
   try {
     const response = await fetch("http://api.sr.se/api/v2/channels?format=json&size=100");
     const data = await response.json();
 
+    //goes through each channel in the data and creates all the html elements necessary for each station
     data.channels.forEach((station) => {
       const stationElement = document.createElement("div");
       stationElement.setAttribute("class", "station");
-
-      stationElement.style.backgroundColor = `#${station.color}`;
 
       stationElement.innerHTML = `
       <div class="station-body">
       <img src="${station.image}" alt="${station.name}" id= "station-image">
       <div class="station-info">
-        <h2 class = "station-name">${station.name}</h2>
-        <p class = "station-desc">${station.tagline}</p>
-        <audio controls  autobuffer>
-          <source src="${station.liveaudio.url}" type="audio/mpeg">
-          Your browser does not support the audio tag.
-        </audio></div></div>
+            <h2 class = "station-name">${station.name}</h2>
+            <p class = "station-desc">${station.tagline}</p>
+          <audio controls  autobuffer>
+            <source src="${station.liveaudio.url}" type="audio/mpeg">
+            Your browser does not support the audio tag.
+      </audio></div></div>
       `;
-
-      stationElement.querySelector('img').addEventListener('click', () => {
-        stationElement.classList.toggle('show-desc');
-      });
       
-      stationElement.querySelector('.station-name').addEventListener('click', () => {
-        stationElement.classList.toggle('show-desc');
-      });
+      //sets backgroundcolor of each station
+      stationElement.style.backgroundColor = `#${station.color}`;
 
-      container.appendChild(stationElement);
-    });
-    
-  } catch (error) {
-    console.error('Error:', error);
+                    //Toggles tagline for stations when pressing logo or name
+                    stationElement.querySelector('img').addEventListener('click', () => {
+                      stationElement.classList.toggle('show-desc');
+                    });
+                    
+                    stationElement.querySelector('.station-name').addEventListener('click', () => {
+                      stationElement.classList.toggle('show-desc');
+                    });
+
+container.appendChild(stationElement);
+});
+} catch (error) {
+  console.error('Error:', error);
   }
 }
-
-// Finally, it's showtime! We're calling our function to start the whole process.
 getRadioStations();
